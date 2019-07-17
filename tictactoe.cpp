4f1ccdee5218dff63432002462db214f0567ab93
Vivek Nathani <52988856+viveknathani@users.noreply.github.com>
2019-07-17 10:06:13 +0530

    first version

---
+++ tictactoe.cpp
@@ -0,0 +1,132 @@
+#include <iostream>
+#include<stdlib.h>
+
+using namespace std;
+
+char matrix[3][3];
+char check();
+void createMatrix();
+void userMove();
+void computerMove();
+void displayMatrix();
+void showCoordinates();
+
+int main()
+{
+    char done;
+    cout<<"This is a tic tac toe game."<<endl;
+    cout<<"You will be playing against the computer."<<endl;
+    cout<<"Below is a coordinate matrix displayed for you."<<endl;
+    showCoordinates();
+    done=' ';
+    createMatrix();
+    cout<<"All the best!"<<endl;
+    system("pause");
+    do{
+        displayMatrix();
+        userMove();
+        done=check();
+        if(done!=' ') { break; }
+        computerMove();
+        done=check();
+    }while(done==' ');
+    if(done=='X') { cout<<"You won!"<<endl; }
+    else { cout<<"I won!"<<endl; }
+    displayMatrix();
+    return 0;
+}
+
+void createMatrix()
+{
+    for(int i=0; i<3; i++)
+    {
+        for(int j=0; j<3; j++)
+        {
+            matrix[i][j]=' ';
+        }
+    }
+}
+
+void userMove()
+{
+    int x, y;
+    cout<<"Enter x,y coordinates for your move."<<endl;
+    cin>>x;
+    cin>>y;
+    cout<<endl;
+    if(matrix[x][y]!=' ')
+    {
+        cout<<"Invalid move. Try again."<<endl;
+        userMove();
+    }
+    else { matrix[x][y]='X'; }
+}
+
+void computerMove()
+{
+    int i,j;
+    for(i=0; i<3; i++)
+    {
+        for(j=0; j<3; j++)
+            if(matrix[i][j]==' ') break;
+        if(matrix[i][j]==' ') break;
+    }
+
+    if(i*j==9)
+    {
+        cout<<"Draw!"<<endl;
+        exit(0);
+    }
+    else
+    {
+        matrix[i][j]='O';
+    }
+}
+
+void showCoordinates()
+{
+    for(int i=0; i<3; i++)
+    {
+        for(int j=0; j<3; j++)
+        {
+            cout<<i<<j<<" ";
+        }
+        cout<<endl;
+        if(i!=2) { cout<<"--|--|--"<<endl; }
+    }
+}
+void displayMatrix()
+{
+    for(int t=0; t<3; t++)
+    {
+        cout<<" "<<matrix[t][0]<<"  "<<matrix[t][1]<<"  "<<matrix[t][2]<<endl;
+        if(t!=2) { cout<<"--|---|--"<<endl; }
+    }
+}
+
+char check()
+{
+    for(int i=0; i<3; i++)
+    {
+        if(matrix[i][0]==matrix[i][1] && matrix[i][0]==matrix[i][2])
+        {
+            return matrix[i][0];
+        }
+    }
+    for(int i=0; i<3; i++)
+    {
+        if(matrix[0][i]==matrix[1][i] && matrix[0][i]==matrix[2][i])
+        {
+            return matrix[0][i];
+        }
+    }
+    if(matrix[0][0]==matrix[1][1] && matrix[0][0]==matrix[2][2])
+    {
+        return matrix[0][0];
+    }
+    if(matrix[1][1]==matrix[0][2] && matrix[1][1]==matrix[2][0])
+    {
+        return matrix[1][1];
+    }
+    return ' ';
+}
